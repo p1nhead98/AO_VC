674b6dee925bc9dbe23c7c1d62e7af8352a5025e
AlfredoOrellana <alf.orellanaa@alumnos.duoc.cl>
2019-11-28 17:25:38 -0400

    Commit 6, login de usuarios

--- miSitio/blog/urls.py
+++ miSitio/blog/urls.py
@@ -9,5 +9,8 @@ urlpatterns = [
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
     path('post/<int:pk>/eliminar/', views.post_delete, name='post_delete'),
+    path('register',views.register),
+    path('login',views.login, name="login"),
+    path('logout',views.logout, name='logout'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- miSitio/blog/views.py
+++ miSitio/blog/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render, get_object_or_404,redirect
+from django.contrib.auth import authenticate
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import login as do_login
+from django.contrib.auth import logout as do_logout
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
@@ -28,17 +32,20 @@ def post_new(request):
 
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    if request.method == "POST":
-        form = PostForm(request.POST, request.FILES, instance=post)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
+    if post.author == request.user:
+        if request.method == "POST":
+            form = PostForm(request.POST, request.FILES, instance=post)
+            if form.is_valid():
+                post = form.save(commit=False)
+                post.author = request.user
             
-            post.published_date = timezone.now()
-            post.save()
-            return redirect('post_detail', pk=post.pk)
+                post.published_date = timezone.now()
+                post.save()
+                return redirect('post_detail', pk=post.pk)
+        else:
+            form = PostForm(instance=post) 
     else:
-        form = PostForm(instance=post) 
+        return redirect('post_detail',pk=post.pk)
     return render(request, 'blog/post_edit.html', {'form': form})
 
 def post_delete(request, pk):
@@ -47,4 +54,32 @@ def post_delete(request, pk):
         post.delete()
         return redirect('post_list')
     return render(request, 'blog/post_delete.html',{'post':post})
+
+def register(request):
+    form = UserCreationForm()
+    if request.method == "POST":
+        form = UserCreationForm(data = request.POST)
+        if form.is_valid():
+            user = form.save()
+            if user is not None:
+                do_login(request, user)
+                return redirect('/')
+    return render(request, "blog/register.html", {'form':form})
+
+def login(request):
+    form=AuthenticationForm()
+    if request.method=="POST":
+        form =AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            username  = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user is not None: 
+                do_login(request,user)
+                return redirect('post_list')
+    return render(request, "blog/login.html", {'form':form})
+
+def logout(request):
+    do_logout(request)
+    return redirect('/')
 
