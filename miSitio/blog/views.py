01834f145b5befcecb767d91d0c3ce9de46eee11
AlfredoOrellana <alf.orellanaa@alumnos.duoc.cl>
2019-12-01 00:25:09 -0400

    8

--- miSitio/blog/views.py
+++ miSitio/blog/views.py
@@ -32,12 +32,12 @@ def post_new(request):
 
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    if post.author == request.user:
+    if post.author == request.user :
         if request.method == "POST":
             form = PostForm(request.POST, request.FILES, instance=post)
             if form.is_valid():
                 post = form.save(commit=False)
-                post.author = request.user
+                
             
                 post.published_date = timezone.now()
                 post.save()
